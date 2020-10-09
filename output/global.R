cf4c1e879bf7686d964ab3ffbb8345421184207b
Mubai Liu <liumubai000@126.com>
2020-10-09 14:52:08 -0400

    add globarl.R

---
+++ output/global.R
@@ -0,0 +1,69 @@
+if (!require("tidyverse")) {
+  install.packages("tidyverse")
+  library(tidyverse)
+}
+if (!require("shinythemes")) {
+  install.packages("shinythemes")
+  library(shinythemes)
+}
+
+if (!require("raster")) {
+  install.packages("raster")
+  library(raster)
+}
+if (!require("RCurl")) {
+  install.packages("RCurl")
+  library(RCurl)
+}
+if (!require("maps")) {
+  install.packages("maps")
+  library(map)
+}
+if (!require("maptools")) {
+  install.packages("maptools")
+  library(maptools)
+}
+if (!require("rgdal")) {
+  install.packages("rgdal")
+  library(rgdal)
+}
+if (!require("leaflet")) {
+  install.packages("leaflet")
+  library(leaflet)
+}
+if (!require("shiny")) {
+  install.packages("shiny")
+  library(shiny)
+}
+if (!require("shinythemes")) {
+  install.packages("shinythemes")
+  library(shinythemes)
+}
+if (!require("plotly")) {
+  install.packages("plotly")
+  library(plotly)
+}
+if (!require("ggplot2")) {
+  install.packages("ggplot2")
+  library(ggplot2)
+}
+if (!require("tigris")) {
+  install.packages("tigris")
+  library(tigris)
+}
+if (!require("geojsonio")) {
+  install.packages("geojsonio")
+  library(geojsonio)
+}
+if (!require("rgdal")) {
+  install.packages("rgdal")
+  library(rgdal)
+}
+if (!require("htmltools")) {
+  install.packages("htmltools")
+  library(htmltools)
+}
+if (!require("sp")) {
+  install.packages("sp")
+  library(sp)
+}
